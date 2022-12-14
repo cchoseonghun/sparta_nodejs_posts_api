d9a5c036c37c16f6a86f9fcf3b8e85025079ae57
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2022-12-14 14:50:52 +0900

    feat: 게시글 수정 기능 구현 refactor: 상황에 맞게 findOne 추가, 수정 및 exec() 추가 refactor: 게시글 조회 작성 날짜 기준 내림차순 정렬 추가

--- routes/posts.js
+++ routes/posts.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const Post = require('../schemas/post');
 
 router.get('/', async (req, res) => {
-  const posts = await Post.find({});
+  const posts = await Post.find().sort('-createdAt').exec();
   let new_posts = posts.map((post) => {
-    let temp = {};
-    temp.postId = post._id;
-    temp.user = post.user;
-    temp.title = post.title;
-    temp.createdAt = post.createdAt;
-    return temp;
+    let new_post = {};
+    new_post.postId = post._id;
+    new_post.user = post.user;
+    new_post.title = post.title;
+    new_post.createdAt = post.createdAt;
+    return new_post;
   })
 
   return res.status(200).json({ data: new_posts });
@@ -20,16 +20,14 @@ router.get('/', async (req, res) => {
 router.get('/:_postId', async (req, res) => {
   const {_postId} = req.params;
 
-  const post = await Post.find({_id: _postId});
-  const [new_post] = post.map((post) => {
-    let temp = {};
-    temp.postId = post._id;
-    temp.user = post.user;
-    temp.title = post.title;
-    temp.content = post.content;
-    temp.createdAt = post.createdAt;
-    return temp;
-  })
+  const post = await Post.findOne({_id: _postId}).exec();
+
+  let new_post = {};
+  new_post.postId = post._id;
+  new_post.user = post.user;
+  new_post.title = post.title;
+  new_post.content = post.content;
+  new_post.createdAt = post.createdAt;
 
   return res.status(200).json({ data: new_post });
 })
@@ -44,8 +42,32 @@ router.post('/', async (req, res) => {
 
   try {
     await Post.create({ user, password, title, content });
-    return res.status(200).json({ message: '게시글을 생성하였습니다.' });
+    return res.status(201).json({ message: '게시글을 생성하였습니다.' });
   } catch {
+    return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
+  }
+})
+
+router.put('/:_postId', async (req, res) => {
+  const {_postId} = req.params;
+  const {password, title, content} = req.body;
+
+  try {
+    const post = await Post.findOne({_id: _postId}).exec();  // 해당 게시글 확인
+    if (!post) {
+      return res.status(404).json({ message: '게시글 조회에 실패하였습니다.' });
+    } else {
+      if (password !== post.password) {  // 비밀번호 체크
+        return res.status(401).json({ message: '게시글 수정 권한이 없습니다.' });
+      } else {
+        await Post.updateOne(
+          {_id: _postId}, 
+          {$set: {password: password, title: title, content: content}}
+        )
+        return res.status(200).json({ message: '게시글을 수정하였습니다.' });
+      }
+    }
+  } catch (err) {
     return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
   }
 })
