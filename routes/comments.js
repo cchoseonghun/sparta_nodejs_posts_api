fc81f5eb24b5563be48b32c43d8f23ec19c444bd
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2022-12-15 19:29:28 +0900

    feat: swagger-autogen을 통해 swagger 기능으로 메인 API 페이지 구현

--- routes/comments.js
+++ routes/comments.js
@@ -4,27 +4,29 @@ const router = express.Router();
 const Comment = require('../schemas/comment');
 const Post = require('../schemas/post');
 
-router.get('/:_postId', async (req, res) => {
-  try {
-    const {_postId} = req.params;
-  
-    const comments = await Comment.find({ postId: _postId }).sort('-createdAt').exec();
-    let new_comments = comments.map((comment) => {
-      let new_comment = {};
-      new_comment.commentId = comment._id;
-      new_comment.user = comment.user;
-      new_comment.content = comment.content;
-      new_comment.createdAt = comment.createdAt;
-      return new_comment;
-    })
-  
-    return res.status(200).json({ data: new_comments });
-  } catch (err) {
-    return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
-  }
-})
-
 router.post('/:_postId', async (req, res) => {
+  //  #swagger.description = '댓글 생성'
+  //  #swagger.tags = ['comments']
+  /*  #swagger.parameters[''] = {
+                  in: 'body',
+                  schema: {
+                      user: 'Developer',
+                      password: '1234',
+                      content: '안녕하세요 댓글 입니다'
+                  }
+  } */
+  /*  #swagger.responses[201] = {
+              description: '댓글 작성 성공',
+              schema: {
+                  message: '댓글을 생성하였습니다.'
+              }
+  } */
+    /*  #swagger.responses[400] = {
+              description: 'body 또는 params를 입력받지 못한 경우',
+              schema: {
+                  message: '데이터 형식이 올바르지 않습니다.'
+              }
+  } */
   try {
     const {_postId} = req.params;
     const {user, password, content} = req.body;
@@ -49,7 +51,81 @@ router.post('/:_postId', async (req, res) => {
   }
 })
 
+router.get('/:_postId', async (req, res) => {
+  //  #swagger.description = '댓글 목록 조회'
+  //  #swagger.tags = ['comments']
+  /*  #swagger.responses[200] = {
+            description: '댓글 목록 조회 성공',
+            schema: {
+                data: [
+                  {
+                    commentId: '639a82521099c5ef762d3698', 
+                    user: 'Developer', 
+                    content: '안녕하세요 댓글입니다.', 
+                    createdAt: '2022-12-14T03:39:20.389Z'
+                  }
+                ]
+            }
+  } */
+    /*  #swagger.responses[400] = {
+              description: 'body 또는 params를 입력받지 못한 경우',
+              schema: {
+                  message: '데이터 형식이 올바르지 않습니다.'
+              }
+  } */
+  try {
+    const {_postId} = req.params;
+  
+    const comments = await Comment.find({ postId: _postId }).sort('-createdAt').exec();
+    let new_comments = comments.map((comment) => {
+      let new_comment = {};
+      new_comment.commentId = comment._id;
+      new_comment.user = comment.user;
+      new_comment.content = comment.content;
+      new_comment.createdAt = comment.createdAt;
+      return new_comment;
+    })
+  
+    return res.status(200).json({ data: new_comments });
+  } catch (err) {
+    return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
+  }
+})
+
 router.put('/:_commentId', async (req, res) => {
+  // #swagger.description = '댓글 수정'
+  //  #swagger.tags = ['comments']
+  /*  #swagger.parameters[''] = {
+                  in: 'body',
+                  schema: {
+                      password: '1234',
+                      content: '수정된 댓글입니다.'
+                  }
+  } */
+  /*  #swagger.responses[200] = {
+              description: '댓글 수정 성공',
+              schema: {
+                  message: '댓글을 수정하였습니다.'
+              }
+  } */
+  /*  #swagger.responses[400] = {
+              description: 'body 또는 params를 입력받지 못한 경우',
+              schema: {
+                  message: '데이터 형식이 올바르지 않습니다.'
+              }
+  } */
+    /*  #swagger.responses[401] = {
+            description: '비밀번호가 일치하지 않는 경우',
+            schema: {
+                message: '댓글 수정 권한이 없습니다.'
+            }
+  } */
+  /*  #swagger.responses[404] = {
+            description: '_commentId에 해당하는 댓글이 존재하지 않을 경우',
+            schema: {
+                message: '댓글 조회에 실패하였습니다.'
+            }
+  } */
   try {
     const {_commentId} = req.params;
     const {password, content} = req.body;
@@ -82,6 +158,38 @@ router.put('/:_commentId', async (req, res) => {
 })
 
 router.delete('/:_commentId', async (req, res) => {
+  // #swagger.description = '댓글 삭제'
+  //  #swagger.tags = ['comments']
+  /*  #swagger.parameters[''] = {
+                  in: 'body',
+                  schema: {
+                      password: '1234',
+                  }
+  } */
+  /*  #swagger.responses[200] = {
+              description: '댓글 삭제 성공',
+              schema: {
+                  message: '댓글을 삭제하였습니다.'
+              }
+  } */
+  /*  #swagger.responses[400] = {
+              description: 'body 또는 params를 입력받지 못한 경우',
+              schema: {
+                  message: '데이터 형식이 올바르지 않습니다.'
+              }
+  } */
+    /*  #swagger.responses[401] = {
+            description: '비밀번호가 일치하지 않는 경우',
+            schema: {
+                message: '댓글 삭제 권한이 없습니다.'
+            }
+  } */
+  /*  #swagger.responses[404] = {
+            description: '_commentId에 해당하는 댓글이 존재하지 않을 경우',
+            schema: {
+                message: '댓글 조회에 실패하였습니다.'
+            }
+  } */
   try {
     const {_commentId} = req.params;
     const {password} = req.body;
--- routes/posts.js
+++ routes/posts.js
@@ -3,40 +3,30 @@ const router = express.Router();
 
 const Post = require('../schemas/post');
 
-router.get('/', async (req, res) => {
-  const posts = await Post.find().sort('-createdAt').exec();
-  let new_posts = posts.map((post) => {
-    let new_post = {};
-    new_post.postId = post._id;
-    new_post.user = post.user;
-    new_post.title = post.title;
-    new_post.createdAt = post.createdAt;
-    return new_post;
-  })
-
-  return res.status(200).json({ data: new_posts });
-})
-
-router.get('/:_postId', async (req, res) => {
-  try {
-    const {_postId} = req.params;
-  
-    const post = await Post.findOne({_id: _postId}).exec();
-  
-    let new_post = {};
-    new_post.postId = post._id;
-    new_post.user = post.user;
-    new_post.title = post.title;
-    new_post.content = post.content;
-    new_post.createdAt = post.createdAt;
-  
-    return res.status(200).json({ data: new_post });
-  } catch (err) {
-    return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
-  }
-})
-
 router.post('/', async (req, res) => {
+  //  #swagger.description = '게시글 작성'
+  //  #swagger.tags = ['posts']
+  /*  #swagger.parameters[''] = {
+                  in: 'body',
+                  schema: {
+                      user: 'Developer',
+                      password: '1234',
+                      title: '안녕하세요', 
+                      content: '안녕하세요 content 입니다'
+                  }
+  } */
+  /*  #swagger.responses[201] = {
+              description: '게시글 작성 성공',
+              schema: {
+                  message: '게시글을 생성하였습니다.'
+              }
+  } */
+  /*  #swagger.responses[400] = {
+              description: 'body 또는 params를 입력받지 못한 경우',
+              schema: {
+                  message: '데이터 형식이 올바르지 않습니다.'
+              }
+  } */
   try {
     const {user, password, title, content} = req.body;
 
@@ -56,7 +46,109 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.get('/', async (req, res) => {
+  //  #swagger.description = '게시글 조회'
+  //  #swagger.tags = ['posts']
+  /*  #swagger.responses[200] = {
+            description: '게시글 조회 성공',
+            schema: {
+                data: [
+                  {
+                    postId: '639a81d11099c5ef762d3688', 
+                    user: 'Developer', 
+                    title: '안녕하세요', 
+                    createdAt: '2022-12-14T03:39:20.389Z'
+                  }
+                ]
+            }
+  } */
+  const posts = await Post.find().sort('-createdAt').exec();
+  let new_posts = posts.map((post) => {
+    let new_post = {};
+    new_post.postId = post._id;
+    new_post.user = post.user;
+    new_post.title = post.title;
+    new_post.createdAt = post.createdAt;
+    return new_post;
+  })
+
+  return res.status(200).json({ data: new_posts });
+})
+
+router.get('/:_postId', async (req, res) => {
+  // #swagger.description = '게시글 상세 조회'
+  // #swagger.tags = ['posts']
+  /*  #swagger.responses[200] = {
+            description: '게시글 상세 조회 성공',
+            schema: {
+                data: {
+                  postId: '639a81d11099c5ef762d3688', 
+                  user: 'Developer', 
+                  title: '안녕하세요', 
+                  content: '안녕하세요 content 입니다.', 
+                  createdAt: '2022-12-14T03:39:20.389Z'
+                }
+            }
+  } */
+  /*  #swagger.responses[400] = {
+            description: 'body 또는 params를 입력받지 못한 경우',
+            schema: {
+                message: '데이터 형식이 올바르지 않습니다.'
+            }
+} */
+  try {
+    const {_postId} = req.params;
+  
+    const post = await Post.findOne({_id: _postId}).exec();
+  
+    let new_post = {};
+    new_post.postId = post._id;
+    new_post.user = post.user;
+    new_post.title = post.title;
+    new_post.content = post.content;
+    new_post.createdAt = post.createdAt;
+  
+    return res.status(200).json({ data: new_post });
+  } catch (err) {
+    return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
+  }
+})
+
 router.put('/:_postId', async (req, res) => {
+  // #swagger.description = '게시글 수정'
+  //  #swagger.tags = ['posts']
+  /*  #swagger.parameters[''] = {
+                  in: 'body',
+                  schema: {
+                      password: '1234',
+                      title: '안녕하세요', 
+                      content: '안녕하세요 content 입니다'
+                  }
+  } */
+  /*  #swagger.responses[200] = {
+              description: '게시글 수정 성공',
+              schema: {
+                  message: '게시글을 수정하였습니다.'
+              }
+  } */
+  /*  #swagger.responses[400] = {
+              description: 'body 또는 params를 입력받지 못한 경우',
+              schema: {
+                  message: '데이터 형식이 올바르지 않습니다.'
+              }
+  } */
+    /*  #swagger.responses[401] = {
+            description: '비밀번호가 일치하지 않는 경우',
+            schema: {
+                message: '게시글 수정 권한이 없습니다.'
+            }
+  } */
+  /*  #swagger.responses[404] = {
+            description: '_postId에 해당하는 게시글이 존재하지 않을 경우',
+            schema: {
+                message: '게시글 조회에 실패하였습니다.'
+            }
+  } */
   try {
     const {_postId} = req.params;
     const {password, title, content} = req.body;
@@ -85,6 +177,38 @@ router.put('/:_postId', async (req, res) => {
 })
 
 router.delete('/:_postId', async (req, res) => {
+  // #swagger.description = '게시글 삭제'
+  //  #swagger.tags = ['posts']
+  /*  #swagger.parameters[''] = {
+                  in: 'body',
+                  schema: {
+                      password: '1234',
+                  }
+  } */
+  /*  #swagger.responses[200] = {
+              description: '게시글 삭제 성공',
+              schema: {
+                  message: '게시글을 삭제하였습니다.'
+              }
+  } */
+  /*  #swagger.responses[400] = {
+              description: 'body 또는 params를 입력받지 못한 경우',
+              schema: {
+                  message: '데이터 형식이 올바르지 않습니다.'
+              }
+  } */
+    /*  #swagger.responses[401] = {
+            description: '비밀번호가 일치하지 않는 경우',
+            schema: {
+                message: '게시글 삭제 권한이 없습니다.'
+            }
+  } */
+  /*  #swagger.responses[404] = {
+            description: '_postId에 해당하는 게시글이 존재하지 않을 경우',
+            schema: {
+                message: '게시글 조회에 실패하였습니다.'
+            }
+  } */
   try {
     const {_postId} = req.params;
     const {password} = req.body;
